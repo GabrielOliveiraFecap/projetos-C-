3182c6c5f7c9be243adb000b3191c02b680783f9
GabrielOliveiraFecap <gabriel.oliveira4@edu.fecap.br>
2026-09-02 19:18:44 -0300

    Exercicio circunferencia

--- obj/Debug/net10.0/projetos C#.AssemblyInfo.cs
+++ obj/Debug/net10.0/projetos C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projetos C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7a0cfff3240be71fb97756ca6c1bde7e9a54f9a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57131647411017cf6cf4f05f567afcbb048544ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("projetos C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projetos C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
